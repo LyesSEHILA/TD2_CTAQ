a353a01eb8c137188c88dc1bfdddeeec96ecc68c
LyesSEHILA <sehilalyes@gmail.com>
2026-09-24 12:42:25 +0200

    TD2-Q01 - Initialisation du projet Gradle

---
+++ app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    java
+    jacoco
+}
+
+group = "org.acme"
+version = "1.0-alpha"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.11.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.hamcrest:hamcrest:2.2")
+    testImplementation("org.mockito:mockito-core:5.14.2")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.14.2")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        html.required.set(true)
+        xml.required.set(true)
+    }
+}
